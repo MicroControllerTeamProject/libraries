43194d9feae2f966b85cf6209adbe372f25da703
Luigi Santagada <luigi.santagada@live.com>
2023-01-10 01:21:07 +0100

    .

--- lsgNewFramework/repository/RFReceiverRepository.h
+++ lsgNewFramework/repository/RFReceiverRepository.h
@@ -5,9 +5,9 @@ class RFReceiverRepository
 public:
 	RFReceiverRepository(uint8_t rx_pin, uint8_t ptt_pin, uint16_t vw_speed);
 	~RFReceiverRepository();
-	void begin();
-	void stop();
-	uint8_t* getMessage();
+	virtual void begin();
+	virtual void stop();
+	virtual uint8_t* getMessage();
 private:
 	uint8_t _rx_pin;
 	uint8_t _ptt_pin;
