42961916b2b0875fccf8cabca5ff566dce0c4ce6
Luigi Santagada <luigi.santagada@live.com>
2023-06-13 14:39:47 +0200

    Modified to manage attiny85 or atmega328 sample.

--- ActivityFramework/Samples/voltageActivitySample/voltageActivitySample.ino
+++ ActivityFramework/Samples/voltageActivitySample/voltageActivitySample.ino
@@ -9,30 +9,61 @@
 #include <AnalogPortSensor.h>
 #include <Arduino.h>
 
-
 AnalogPort listOfBatteryAnalogPorts[1];
 AvrMicroRepository avrMicroRepository(19200);
 AnalogPortSensor analogSensor;
 VoltageActivity batteryVoltageActivity;
 
-void setup() {
+void setup()
+{
+#if defined(__AVR_ATmega328P__)
 	Serial.begin(9600);
-	listOfBatteryAnalogPorts[0] =  AnalogPort("BA0", A1);
+#endif
+
+#if defined(__AVR_ATmega328P__)
+  #define pin_Led 13;
+  pinMode(13, OUTPUT);
+#else if defined (__AVR_ATtiny85__)
+  pinMode(0, OUTPUT);
+  #define pin_Led 0;
+#endif
+	
+	listOfBatteryAnalogPorts[0] = AnalogPort("BA0", 4);
 	listOfBatteryAnalogPorts[0].isEnable = true;
 	listOfBatteryAnalogPorts[0].maxVoltageAlarmValueIn = 4.30f;
 	listOfBatteryAnalogPorts[0].minVoltageAlarmValueIn = 3.25f;
-	analogSensor = AnalogPortSensor("SB01",listOfBatteryAnalogPorts, sizeof(listOfBatteryAnalogPorts) / sizeof(listOfBatteryAnalogPorts[0]));
-	batteryVoltageActivity = VoltageActivity(avrMicroRepository, analogSensor, 4.80f, commonsLayer::DEFAULT_m);
+	analogSensor = AnalogPortSensor("SB01", listOfBatteryAnalogPorts, sizeof(listOfBatteryAnalogPorts) / sizeof(listOfBatteryAnalogPorts[0]));
+	batteryVoltageActivity = VoltageActivity(avrMicroRepository, analogSensor, 3.70f, commonsLayer::DEFAULT_m);
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() {
- 
-	Serial.print("voltage : "); Serial.println(batteryVoltageActivity.getVoltage("BA0"));
-	Serial.print("graph : "); Serial.println(batteryVoltageActivity.getGrafBarLevel("BA0",3.30,3.80,4.30));
+void loop()
+{
+#if defined(__AVR_ATmega328P__)
+	Serial.print("voltage : ");
+	Serial.println(batteryVoltageActivity.getVoltage("BA0"));
+	Serial.print("graph : ");
+	Serial.println(batteryVoltageActivity.getGrafBarLevel("BA0", 3.30, 3.80, 4.30));
+#endif
 	if (batteryVoltageActivity.isVoltageOutOfRange("BA0"))
 	{
+    #if defined(__AVR_ATmega328P__)
+		digitalWrite(13, HIGH);
+		delay(1000);
+		digitalWrite(13, LOW);
+		delay(1000);
+    #endif
+
+    #if defined(__AVR_ATtiny85__)
+		digitalWrite(0, HIGH);
+		delay(1000);
+		digitalWrite(0, LOW);
+		delay(1000);
+    #endif
+
+		#if defined(__AVR_ATmega328P__)
 		Serial.println("problem");
+		#endif
 	}
 	delay(500);
 }
