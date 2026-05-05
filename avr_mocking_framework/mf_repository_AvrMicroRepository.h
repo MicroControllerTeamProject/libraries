6a9e37aec409cae0282f3964a95d4e5b5621aa7a
luigi.santagada@live.com <luigi.santagada@live.com>
2026-05-05 23:46:03 +0200

    .

--- avr_mocking_framework/mf_repository_AvrMicroRepository.h
+++ avr_mocking_framework/mf_repository_AvrMicroRepository.h
@@ -6,6 +6,7 @@
 class AvrMicroRepository {
 public:
     AvrMicroRepository(ISerial& serial, mf::commons::commonsLayer::AnalogRefMode vref_mode, float vref);
+#if _ON_MOCKING_TESTS
     // --- Funzioni analogiche ---
     virtual void analogReference(uint8_t mode);
     virtual uint16_t analogRead(uint8_t analogPin);
@@ -44,6 +45,46 @@ public:
     virtual unsigned long get_millis();
     virtual unsigned long pulseIn(uint8_t pin, uint8_t level, unsigned long time = 1000000UL);
     virtual float get_vref();
+#else
+    // --- Funzioni analogiche ---
+    void analogReference(uint8_t mode);
+    uint16_t analogRead(uint8_t analogPin);
+    float analogVoltageRead(uint8_t analogPin);
+    // --- Funzioni digitali ---
+    uint8_t digitalRead(uint8_t pin);
+    void digitalWrite(uint8_t pin, uint8_t value);
+    void pinMode(uint8_t pin, uint8_t mode);
+    // --- Stato configurazione pin ---
+    bool isPinOutput(uint8_t pin);
+    bool isPinInput(uint8_t pin);
+    bool isPinInputPullup(uint8_t pin);
+    // --- Timing e memoria ---
+    void delay(unsigned long delayTime);
+    void delayMicroseconds(unsigned int micSec);
+    int getFreeRam();
+    void free(void* ptr);
+    // --- Seriale: stampa e lettura ---
+    void print(const char* data);
+    void print(float data);
+    void print(int32_t data);
+    void print(uint8_t data);
+    void print(char data);
+    void println(const char* data);
+    void println(float data);
+    void println(int32_t data);
+    void println(uint8_t data);
+    void println(char data);
+    int available();
+    size_t readString(char* buffer, size_t maxLen);
+    void clearBuffer();
+    int getSerialBufferSize();
+    void begin(unsigned long baud);
+    int read();
+    ~AvrMicroRepository();
+    unsigned long get_millis();
+    unsigned long pulseIn(uint8_t pin, uint8_t level, unsigned long time = 1000000UL);
+    float get_vref();
+#endif
 private:
     ISerial& serial_;
     mf::commons::commonsLayer::AnalogRefMode vref_mode = mf::commons::commonsLayer::AnalogRefMode::DEFAULT_m;
